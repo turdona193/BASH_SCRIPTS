ea8b0b9c86d6e6a6fa66118680af7c5c655bd1a6
Nicholas Turdo <turdona193@potsdam.edu>
2016-12-21 17:20:59 -0500

    Update for COE_TESTING_ExternalConnectivity.sh

--- COE_TESTING_ExternalConnectivity.sh
+++ COE_TESTING_ExternalConnectivity.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-COE_TESTING_InboundConnectivity.sh -s -p=COE_TESTING_ExternalConnectivity_Localhost_FTP.properties FTP
-COE_TESTING_InboundConnectivity.sh -s -p=COE_TESTING_ExternalConnectivity_Localhost_SFTP.properties SFTP
+bash COE_TESTING_InboundConnectivity.sh -s -p=COE_TESTING_ExternalConnectivity_Localhost_FTP.properties FTP
+bash COE_TESTING_InboundConnectivity.sh -s -p=COE_TESTING_ExternalConnectivity_Localhost_SFTP.properties SFTP
